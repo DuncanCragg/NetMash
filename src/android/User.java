9d89cb5759c18773fec52cc66b43d1de20ecd580
Duncan Cragg <netmash@cilux.org>
2011-04-07 22:28:45 +0100

    GPS part one

--- src/android/User.java
+++ src/android/User.java
@@ -5,6 +5,7 @@ import java.util.*;
 import java.util.regex.*;
 
 import android.gui.*;
+import android.os.*;
 
 import android.content.Context;
 import android.database.Cursor;
@@ -15,6 +16,8 @@ import android.location.*;
 import static android.provider.Contacts.*;
 import static android.provider.Contacts.ContactMethods.*;
 import static android.provider.Contacts.ContactMethodsColumns.*;
+import static android.content.Context.*;
+import static android.location.LocationManager.*;
 
 import netmash.lib.JSON;
 import netmash.forest.WebObject;
@@ -28,9 +31,12 @@ public class User extends WebObject {
 
     static public User me=null;
 
+    CurrentLocation currentlocation;
+
     public User(){ if(me==null){ me=this; if(NetMash.top!=null) NetMash.top.onUserReady(this); } }
 
     public void onTopCreate(){
+        currentlocation = new CurrentLocation(this);
     }
 
     public void onTopResume(){ logrule();
@@ -39,12 +45,20 @@ public class User extends WebObject {
                 showWhatIAmViewing();
             }
         };
+        currentlocation.getLocationUpdates();
     }
 
     public void onTopPause(){
+        currentlocation.stopLocationUpdates();
     }
 
     public void onTopDestroy(){
+    }
+
+    // ---------------------------------------------------------
+
+    void onNewLocation(Location location){
+        log("onNewLocation: "+location);
     }
 
     // ---------------------------------------------------------
@@ -444,6 +458,81 @@ public class User extends WebObject {
             else log("No getFromLocationName for "+address);
         }catch(Exception e){ log("No getFromLocationName for "+address); log(e); }
         return null; 
+    }
+
+    // ---------------------------------------------------------
+
+    private class CurrentLocation implements LocationListener {
+
+        private static final int SECONDS = 1000;
+        private static final int MINUTES = 60 * SECONDS;
+
+        private User user;
+        private LocationManager locationManager;
+        private Location currentLocation;
+
+        CurrentLocation(User user){
+            this.user=user;
+            locationManager=(LocationManager)NetMash.top.getSystemService(LOCATION_SERVICE);
+        }
+
+        public void getLocationUpdates(){
+            Location netloc=locationManager.getLastKnownLocation(NETWORK_PROVIDER);
+            Location gpsloc=locationManager.getLastKnownLocation(GPS_PROVIDER);
+            currentLocation = isBetterLocation(netloc, gpsloc)? netloc: gpsloc;
+            if(currentLocation!=null) user.onNewLocation(currentLocation);
+            locationManager.requestLocationUpdates(NETWORK_PROVIDER, 15*SECONDS, 0, this);
+            locationManager.requestLocationUpdates(GPS_PROVIDER,     15*SECONDS, 0, this);
+        }
+
+        public void stopLocationUpdates(){
+            locationManager.removeUpdates(this);
+        }
+
+        public void onLocationChanged(Location location){
+            if(!isBetterLocation(location, currentLocation)) return;
+            if( hasMovedLocation(location, currentLocation)) user.onNewLocation(location);
+            currentLocation=location;
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras){}
+        public void onProviderEnabled(String provider){}
+        public void onProviderDisabled(String provider){}
+
+        protected boolean isBetterLocation(Location newLocation, Location prevLocation) {
+
+            if(newLocation ==null) return false;
+            if(prevLocation==null) return true; 
+
+            long timeDelta = newLocation.getTime() - prevLocation.getTime();
+            boolean isSignificantlyNewer = timeDelta >  (2*MINUTES);
+            boolean isSignificantlyOlder = timeDelta < -(2*MINUTES);
+            boolean isNewer              = timeDelta > 0;
+            if(isSignificantlyNewer) return true;
+            if(isSignificantlyOlder) return false;
+
+            int accuracyDelta=(int)(newLocation.getAccuracy() - prevLocation.getAccuracy());
+            boolean isMoreAccurate              = accuracyDelta < 0;
+            boolean isLessAccurate              = accuracyDelta > 0;
+            boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+            boolean isFromSameProvider = isSameProvider(newLocation.getProvider(), prevLocation.getProvider());
+            if(isMoreAccurate) return true;
+            if(isNewer && !isLessAccurate) return true;
+            if(isNewer && !isSignificantlyLessAccurate && isFromSameProvider) return true;
+            return false;
+        }
+
+        private boolean hasMovedLocation(Location newLocation, Location prevLocation){
+            if(prevLocation==null) return newLocation!=null;
+            if(newLocation.getLatitude() !=prevLocation.getLatitude() ) return true;
+            if(newLocation.getLongitude()!=prevLocation.getLongitude()) return true;
+            return false;
+        }
+
+        private boolean isSameProvider(String provider1, String provider2){
+            if(provider1==null) return provider2==null;
+            return provider1.equals(provider2);
+        }
     }
 
     // ---------------------------------------------------------
