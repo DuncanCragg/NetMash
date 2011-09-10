38e6eb1e3da9383de6f8eff51373bc0c6bd0058b
Duncan Cragg <netmash@cilux.org>
2011-09-10 08:11:02 +0100

    minor fix and log

---
+++ src/netmash/platform/Logging.java
@@ -0,0 +1,13 @@
+
+package netmash.platform;
+
+/** import static netmash.platform.Logging.*;
+*/
+public class Logging{
+
+    static public int L(String t){
+        System.out.println(t);
+        return 1;
+    }
+}
+
