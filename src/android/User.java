677b54ad093057a64ed70bd85df255dc0e5a3d3c
Duncan Cragg <object.network@cilux.org>
2012-10-19 11:09:28 +0100

    extract maths game to topdb.json

--- src/android/User.java
+++ src/android/User.java
@@ -61,118 +61,6 @@ public class User extends WebObject {
 
         // -----------------------------------------------------
 
-        Editable lightrule1 = new Editable(
-              "{ \"is\": [ \"editable\", \"rule\" ], \n"+
-              "  \"when\": \"swiped down, turn on light\", \n"+
-              "  \"%alerted\": { \"is\": \"swipe\", \"dy\": [ \">\", 0 ] }, \n"+
-              "  \"light\": [ \"*\", [ \"=>\", 1  ], [ \"=>\", 1 ] ] \n"+
-              "}");
-
-        Editable lightrule2= new Editable(
-              "{ \"is\": [ \"editable\", \"rule\" ], \n"+
-              "  \"when\": \"swiped up, turn off light\", \n"+
-              "  \"%alerted\": { \"is\": \"swipe\", \"dy\": [ \"<\", 0 ] }, \n"+
-              "  \"light\": [ \"*\", [ \"=>\", 0.4  ], [ \"=>\", 0.7 ] ] \n"+
-              "}");
-
-        Editable gamerulec = new Editable(
-              "{ \"is\": [ \"editable\", \"rule\" ], \n"+
-              "  \"when\": \"starting, create lights\", \n"+
-              "  \"subObjects\": [ [ \"number\", \"=>\",\n"+
-              "      { \"object\": \n"+
-              "          { \"%uid\": \"new\", \n"+
-              "            \"%rules\": \"$:gamerules\", \n"+
-              "            \"is\": [ \"3d\", \"cuboid\", \"editable\" ], \n"+
-              "            \"title\": \"Game Cube\", \n"+
-              "            \"rotation\": [ 0, 0, 0 ], \n"+
-              "            \"scale\": [ 0.5, 0.5, 0.5 ], \n"+
-              "            \"light\": [ 0.0, 0.4, 0.7 ] \n"+
-              "          }, \n"+
-              "        \"coords\": [ \"$:!\", -1,  0 ] \n"+
-              "      } \n"+
-              "  ] ] \n"+
-              "}");
-
-        Editable gameruler = new Editable(
-              "{ \"is\": [ \"editable\", \"rule\" ], \n"+
-              "  \"when\": \"swiped right, set to checking\", \n"+
-              "  \"%alerted\": { \"is\": \"swipe\", \"dx\": [ \">\", 0 ] }, \n"+
-              "  \"status\": [ \"=>\", \"checking\" ] \n"+
-              "}");
-
-        Editable gamerules = new Editable(
-              "{ \"is\": [ \"editable\", \"rule\" ], \n"+
-              "  \"when\": \"swiped left after sorry, set to waiting with old challenge\", \n"+
-              "  \"%alerted\": { \"is\": \"swipe\", \"dx\": [ \"<\", 0 ] }, \n"+
-              "  \"status\": [ \"sorry\", \"=>\", \"waiting\" ] \n"+
-              "}");
-
-        Editable gameruleg = new Editable(
-              "{ \"is\": [ \"editable\", \"rule\" ], \n"+
-              "  \"when\": \"swiped left after good, set to waiting plus new challenge\", \n"+
-              "  \"%alerted\": { \"is\": \"swipe\", \"dx\": [ \"<\", 0 ] }, \n"+
-              "  \"status\": [ \"good\", \"=>\", \"waiting\" ], \n"+
-              "  \"numerator\": [ \"=>\", \"random\", 0, 6 ] \n"+
-              "}");
-
-        Editable gamerulew = new Editable(
-              "{ \"is\": [ \"editable\", \"rule\" ], \n"+
-              "  \"when\": \"waiting, set text to challenge\", \n"+
-              "  \"status\": \"waiting\", \n"+
-              "  \"text\": [ \"*\", [ \"=>\", \"$:numerator\" ], [ \"=>\", \"Swipe right when done ->\" ] ], \n"+
-              "  \"light\": [ [ \"=>\", 0.5  ], [ \"=>\", 0.5 ], [ \"=>\", 1.0 ] ] \n"+
-              "}");
-
-        Editable gamerule0 = new Editable(
-              "{ \"is\": [ \"editable\", \"rule\" ], \n"+
-              "  \"when\": \"checking, set to initial zero\", \n"+
-              "  \"status\": \"checking\", \n"+
-              "  \"lit\": [ \"=>\", 0 ] \n"+
-              "}");
-
-        Editable gamerule1 = new Editable(
-              "{ \"is\": [ \"editable\", \"rule\" ], \n"+
-              "  \"when\": \"checking, count lit up lights\", \n"+
-              "  \"status\": \"checking\", \n"+
-              "  \"subObjects\": [ { \"object\": { \"light\": [ \"*\", 1, 1 ] } } ], \n"+
-              "  \"lit\": [ \"=>\", \"count\", \"$::subObjects\" ] \n"+
-              "}");
-
-        Editable gamerulen = new Editable(
-              "{ \"is\": [ \"editable\", \"rule\" ], \n"+
-              "  \"when\": \"checking, if incorrect, set text\", \n"+
-              "  \"status\": [ \"checking\", \"=>\", \"sorry\" ], \n"+
-              "  \"lit\": \"!$:numerator\", \n"+
-              "  \"text\": [ \"*\", \"*\", [ \"=>\", \"Sorry.. Swipe left <-\" ] ], \n"+
-              "  \"light\": [ [ \"=>\", 1.0  ], [ \"=>\", 0.5 ], [ \"=>\", 0.5 ] ] \n"+
-              "}");
-
-        Editable gameruley = new Editable(
-              "{ \"is\": [ \"editable\", \"rule\" ], \n"+
-              "  \"when\": \"checking, if correct, set text\", \n"+
-              "  \"status\": [ \"checking\", \"=>\", \"good\" ], \n"+
-              "  \"lit\": \"$:numerator\", \n"+
-              "  \"text\": [ \"*\", \"*\", [ \"=>\", \"Good! Swipe left <-\" ] ], \n"+
-              "  \"light\": [ [ \"=>\", 0.5  ], [ \"=>\", 1.0 ], [ \"=>\", 0.5 ] ] \n"+
-              "}");
-
-        Editable gamesign = new Editable(
-              "{ \"%rules\": [ \""+gamerulec.uid+"\", \""+gameruler.uid+"\", \""+gamerules.uid+"\", \""+gameruleg.uid+"\", \""+gamerulew.uid+"\", \""+gamerule0.uid+"\", \""+gamerule1.uid+"\", \""+gamerulen.uid+"\", \""+gameruley.uid+"\" ], \n"+
-              "  \"is\": [ \"3d\", \"notice\", \"editable\" ], \n"+
-              "  \"title\": \"Maths Game\", \n"+
-              "  \"text\": [ \"Light up this fraction:\", \"-\", \"-\" ], \n"+
-              "  \"rotation\": [ 0, 0, 0 ], \n"+
-              "  \"scale\": [ 1.0, 1.0, 1.0 ], \n"+
-              "  \"light\": [ 0.5, 0.5, 1.0 ], \n"+
-              "  \"subObjects\": [ -2.75, -1.65, -0.55, 0.55, 1.65, 2.75 ], \n"+
-              "  \"gamerules\": [ \""+lightrule1.uid+"\", \""+lightrule2.uid+"\" ], \n"+
-              "  \"numerator\": 3, \n"+
-              "  \"status\": \"waiting\", \n"+
-              "  \"lit\": 0 \n"+
-              "}");
-
-        // -----------------------------------------------------
-
         String homeusers=Kernel.config.stringPathN("ots:homeusers");
         me = new User(homeusers, contact.uid, links.uid, contacts.uid);
 
@@ -182,18 +70,6 @@ public class User extends WebObject {
         me.funcobs.cacheSaveAndEvaluate(contact, true);
         me.funcobs.cacheSaveAndEvaluate(links);
         me.funcobs.cacheSaveAndEvaluate(contacts);
-        me.funcobs.cacheSaveAndEvaluate(lightrule1);
-        me.funcobs.cacheSaveAndEvaluate(lightrule2);
-        me.funcobs.cacheSaveAndEvaluate(gamerulec);
-        me.funcobs.cacheSaveAndEvaluate(gameruler);
-        me.funcobs.cacheSaveAndEvaluate(gamerules);
-        me.funcobs.cacheSaveAndEvaluate(gameruleg);
-        me.funcobs.cacheSaveAndEvaluate(gamerulew);
-        me.funcobs.cacheSaveAndEvaluate(gamerule0);
-        me.funcobs.cacheSaveAndEvaluate(gamerule1);
-        me.funcobs.cacheSaveAndEvaluate(gamerulen);
-        me.funcobs.cacheSaveAndEvaluate(gameruley);
-        me.funcobs.cacheSaveAndEvaluate(gamesign);
         me.funcobs.cacheSaveAndEvaluate(me, true);
 
         if(homeusers!=null) me.notifying(list(homeusers));
