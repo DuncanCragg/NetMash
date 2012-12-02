cd71cd2f3ff8fd2766afab9db1b3b12de26eb15d
Duncan Cragg <object.network@cilux.org>
2012-12-02 21:33:25 +0000

    simpler PresenceTracker .. but still just as slow

--- src/netmash/types/PresenceTracker.java
+++ src/netmash/types/PresenceTracker.java
@@ -11,52 +11,39 @@ public class PresenceTracker extends ObjectMash {
     public PresenceTracker(){}
 
     public void evaluate(){
-        if(contentListContains("is", "3d")){
-            trackPresence();
-        }
+        trackPresence();
+        contentAll("sub-objects:object:is");
         super.evaluate();
     }
 
     @SuppressWarnings("unchecked")
-    private void trackPresence(){ logrule();
-
-        LinkedList subuids=contentAll("sub-objects:object");
-        for(String alerted: alerted()){
-            contentTemp("Alerted", alerted);
-            if(contentIsThis("Alerted:place") && !subuids.contains(alerted)){
-                LinkedHashMap hm=new LinkedHashMap();
-                hm.put("object", alerted);
-                hm.put("coords", list(0,0,0));
-                contentListAdd("sub-objects", hm);
-                contentInc("present");
-            }
-            contentTemp("Alerted", null);
-        }
-        LinkedList subObjects=contentList("sub-objects");
-        if(subObjects==null) return;
-        int sosize=subObjects.size();
-        for(int i=0; i<sosize; i++){
-            String placepath=String.format("sub-objects:%d:object:place",i);
-            if(!contentSet(placepath)) continue;
-            if(contentIsThis(placepath)){
-                String coordpath=String.format("sub-objects:%d:object:coords",i);
-                String mycrdpath=String.format("sub-objects:%d:coords",i);
-                LinkedList coords=contentListClone(coordpath);
-                if(coords!=null && coords.size()==3){
-                    LinkedList mycoords=contentList(mycrdpath);
-                    if(mycoords==null) contentList(mycrdpath,coords);
-                    else if(!mycoords.equals(coords) && mycoords.size()==3){
-                        contentObject(mycrdpath+":0", coords.get(0));
-                        contentObject(mycrdpath+":1", coords.get(1));
-                        contentObject(mycrdpath+":2", coords.get(2));
-                    }
+    private void trackPresence(){
+        for(String trackuid: alerted()){
+            contentTemp("Alerted", trackuid);
+            if(!contentSet("Alerted:place")) continue;
+            LinkedList subuids=contentAll("sub-objects:object");
+            int i=subuids.indexOf(trackuid);
+            if(contentIsThis("Alerted:place")){
+                LinkedList coords=contentListClone("Alerted:coords");
+                if(coords==null) coords=list(0,0,0);
+                if(i== -1){
+                    LinkedHashMap hm=new LinkedHashMap();
+                    hm.put("object", trackuid);
+                    hm.put("coords", coords);
+                    contentListAdd("sub-objects", hm);
+                    contentInc("present");
+                }
+                else{
+                    contentList(String.format("sub-objects:%d:coords",i),coords);
                 }
             }
             else{
-                contentListRemove("sub-objects", i);
-                contentDec("present");
-                i--; sosize--;
+                if(i!= -1){
+                    contentListRemove("sub-objects", i);
+                    contentDec("present");
+                }
             }
+            contentTemp("Alerted", null);
         }
     }
 }
