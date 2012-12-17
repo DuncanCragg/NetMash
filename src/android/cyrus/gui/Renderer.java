65507108dccb660056c4a781523240efe2c859f7
Duncan Cragg <object.network@cilux.org>
2012-12-17 22:28:38 +0000

    adjust default light

--- src/android/cyrus/gui/Renderer.java
+++ src/android/cyrus/gui/Renderer.java
@@ -245,7 +245,7 @@ public class Renderer implements GLSurfaceView.Renderer {
             if(!lightObject){
                 float[] poscol;
                 if(lights.size()!=0) poscol=nearestLight(tx, ty, tz);
-                else                 poscol=new float[]{ 0f,10f,0f, 10f,10f,10f };
+                else                 poscol=new float[]{ 3f,7f,3f, 10f,10f,10f };
                 lightPosWorld[0]=poscol[0]; lightPosWorld[1]=poscol[1]; lightPosWorld[2]=poscol[2]; lightPosWorld[3]=1;
                 lightCol[0]     =poscol[3]; lightCol[1]     =poscol[4]; lightCol[2]     =poscol[5];
                 Matrix.multiplyMV(lightPos, 0, matrixVVV, 0, lightPosWorld, 0);
