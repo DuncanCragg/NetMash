355353a4f82ef2e1ff6c6bf96c19f494be849eb2
Duncan Cragg <object.network@cilux.org>
2013-04-02 09:22:23 +0100

    Minecraft Mod

---
+++ src/minecraft/CyrusWorld.java
@@ -0,0 +1,44 @@
+package net.minecraft.src;
+
+import java.util.LinkedList;
+import cyrus.forest.WebObject;
+
+import static cyrus.lib.Utils.*;
+
+import net.minecraft.server.MinecraftServer;
+
+public class CyrusWorld extends WebObject {
+
+    private World world=null;
+
+    public CyrusWorld(){ }
+
+    public World world(){ return MinecraftServer.getServer().worldServerForDimension(0); }
+
+    public void evaluate(){
+        super.evaluate();
+        for(String alerted: alerted()){
+            contentTemp("Alerted", alerted);
+            if(contentListContainsAll("Alerted:is",list("minecraft","spell"))){
+                LinkedList placing=contentList("Alerted:placing");
+                if(placing!=null){
+                    if(placing.size()==5 && placing.get(1).equals("at")){
+                        int x=(int)findNumberIn(placing.get(2));
+                        int y=(int)findNumberIn(placing.get(3));
+                        int z=(int)findNumberIn(placing.get(4));
+                        String name=placing.get(0).toString();
+                        ensureBlockAt(x,y,z,name);
+                    }
+                }
+            }
+            contentTemp("Alerted", null);
+        }
+    }
+
+    private void ensureBlockAt(int x, int y, int z, String name){
+        if(world()==null) return;
+        int id=name.equals("cobblestone")? 4: 1;
+        if(id!=world().getBlockId(x,y,z)) world().setBlock(x,y,z, id);
+    }
+}
+
---
+++ src/minecraft/mod_Cyrus.java
@@ -0,0 +1,12 @@
+package net.minecraft.src;
+
+import cyrus.Cyrus;
+
+public class mod_Cyrus extends BaseMod {
+
+    public String getVersion(){ return "Cyrus Minecraft Mod 0.01"; }
+
+    public void load(){ Cyrus.main(new String[]{}); }
+}
+
+
