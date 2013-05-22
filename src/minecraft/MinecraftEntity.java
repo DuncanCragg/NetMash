0f42fb917a135fd8b16252afa76fe5950ca77299
Duncan Cragg <object.network@cilux.org>
2013-05-22 21:39:47 +0100

    fix player changing on respawn bug

--- src/minecraft/MinecraftEntity.java
+++ src/minecraft/MinecraftEntity.java
@@ -9,6 +9,7 @@ import cyrus.forest.WebObject;
 import static cyrus.lib.Utils.*;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.server.MinecraftServer;
 
 public class MinecraftEntity extends CyrusLanguage implements mod_Cyrus.Tickable {
 
@@ -33,24 +34,41 @@ public class MinecraftEntity extends CyrusLanguage implements mod_Cyrus.Tickable
 
     private double x=30000000,y=30000000,z=30000000;
 
+    boolean thePlayer=false;
+    int currentplayer=0;
+
     public void tick(float var1, Minecraft minecraft){
+        final boolean[] newplayer={false};
+        if(entity instanceof EntityPlayer){
+            EntityPlayer player=minecraft.thePlayer;
+            if(entity==player) thePlayer=true;
+            if(thePlayer){
+                int i=System.identityHashCode(player);
+                if(i!=currentplayer){ currentplayer=i; entity=player; newplayer[0]=true; }
+            }
+        }
         double px=entity.posX;
         double py=entity.posY;
         double pz=entity.posZ;
-        if(vvdist(list(x,y,z),list(px,py,pz)) >= 1){
+        if(vvdist(list(x,y,z),list(px,py,pz)) >= 0.5){
             x=px; y=py; z=pz;
             new Evaluator(this){ public void evaluate(){ try{
                 contentList("position",list((int)(x-1.0),(int)(y-2.5),(int)(z-1.0)));
                 if(entity instanceof EntityPlayer){
-                    EntityPlayer ep=(EntityPlayer)entity;
-                    if(!contentSet("spawn-position")){
-                        ChunkCoordinates spawnpos=ep.getBedLocation();
-                        contentList("spawn-position",list(spawnpos.posX,spawnpos.posY,spawnpos.posZ));
+                    EntityPlayer player=(EntityPlayer)entity;
+                    if(!contentSet("spawn-position") || newplayer[0]){
+                        ChunkCoordinates spawnpos=world().getSpawnPoint();
+                        if(spawnpos!=null) contentList("spawn-position",list(spawnpos.posX,spawnpos.posY,spawnpos.posZ));
                     }
                 }
                 self.evaluate();
             }catch(Exception e){ e.printStackTrace(); } refreshObserves(); }};
         }
+    }
+
+    public World world(){
+        MinecraftServer server=MinecraftServer.getServer();
+        return server==null? null: server.worldServerForDimension(0);
     }
 }
 
