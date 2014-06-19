e16b5ebc5c32f9eea731aa2978b444c19a3b50f0
DuncanCragg <cyrus@cilux.org>
2014-06-19 23:15:09 +0100

    * or × everywhere in Cyrus

--- src/cyrus/forest/CyrusLanguage.java
+++ src/cyrus/forest/CyrusLanguage.java
@@ -753,12 +753,7 @@ public class CyrusLanguage extends WebObject {
             if(d2==null) d2=findDouble(ll.get(2));
             if(d0!=null && d2!=null) return Double.valueOf(d0 + d2);
         }
-        if(ll.size()==3 && "×".equals(s1)){
-            if(d0==null) d0=findDouble(ll.get(0));
-            if(d2==null) d2=findDouble(ll.get(2));
-            if(d0!=null && d2!=null) return Double.valueOf(d0 * d2);
-        }
-        if(ll.size()==3 && "*".equals(s1)){
+        if(ll.size()==3 && ("*".equals(s1) || "×".equals(s1))){
             if(d0==null) d0=findDouble(ll.get(0));
             if(d2==null) d2=findDouble(ll.get(2));
             if(d0!=null && d2!=null) return Double.valueOf(d0 * d2);
@@ -778,7 +773,7 @@ public class CyrusLanguage extends WebObject {
             if(d2==null) d2=findDouble(ll.get(2));
             if(d0!=null && d2!=null) return Double.valueOf(d0 % d2);
         }
-        if(ll.size()==3 && "×".equals(s1)){
+        if(ll.size()==3 && ("*".equals(s1) || "×".equals(s1))){
             if(l0==null) l0=findList(ll.get(0));
             if(l2==null) l2=findList(ll.get(2));
             if(l0!=null && l2!=null){
@@ -815,7 +810,7 @@ public class CyrusLanguage extends WebObject {
             if(l2==null) l2=findList(ll.get(2));
             if(l0!=null && l2!=null) return vvdist(l0, l2);
         }
-        if(ll.size()==3 && "*".equals(s1)){
+        if(ll.size()==3 && ("*".equals(s1) || "×".equals(s1))){
             if(l0==null) l0=findList(ll.get(0));
             if(d2==null) d2=findDouble(ll.get(2));
             if(l0!=null && d2!=null){
