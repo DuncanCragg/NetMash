cf486073add5ce7dabdb29094ef82b3945b64efe
DuncanCragg <cyrus@cilux.org>
2014-07-10 11:52:56 +0100

    new hcitool commands to set adv rate, etc.

--- src/server/vm2/PiBeaconLight.java
+++ src/server/vm2/PiBeaconLight.java
@@ -54,9 +54,6 @@ public class PiBeaconLight extends CyrusLanguage {
         String re="uid-([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])-([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])-([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])-([0-9a-f][0-9a-f])([0-9a-f][0-9a-f])";
         Matcher m = Pattern.compile(re).matcher(uid);
         if(!m.matches()) return;
-                    // hcitool -i hci0 cmd 0x08 0x0008 1e 02 01 1a 1a ff 4c 00 02 15 e2 c5 6d b5 df fb 48 d2 b0 60 d0 f5 a7 10 96 e0 00 00 00 00 c5 00
-                    // hcitool -i hci0 cmd 0x08 0x0008 1e 02 01 1a 1a ff 4c 00 02 15 c0 a8 00 11 1f 92 c0 93 a9 08 a9 d8 f1 c1 00 00 00 00 00 00 00 00
-                    // hcitool -i hci0 cmd 0x08 0x0008 1f 02 01 1a 1b ff 4c 00 02 16 c0 a8 00 12 1f 92 c0 93 a9 08 a9 d8 f1 c1 00 00 00 00 00 00 00 00
         String advert="hcitool -i hci0 cmd 0x08 0x0008 1c 02 01 1a 18 ff 4c 00 02 15 ";
         advert=advert+String.format("%02x %02x %02x %02x %02x %02x %s %s %s %s %s %s %s %s ",
                                      ipbytes[0], ipbytes[1], ipbytes[2], ipbytes[3],
@@ -65,11 +62,13 @@ public class PiBeaconLight extends CyrusLanguage {
         advert=advert+"00 00 00 00 00";
         exec("hciconfig hci0 down");
         exec("hciconfig hci0 up");
-        exec("hciconfig hci0 noleadv");
         exec(advert);
-        exec("hciconfig hci0 leadv");
-        exec(advert); // override dodgy Ubuntu crap set on leadv
-        exec("hciconfig hci0 noscan"); // stop more Ubuntu shite
+        // set rate http://stackoverflow.com/questions/21124993/is-there-a-way-to-increase-ble-advertisement-frequency-in-bluez
+        //                                    0x0300*0.625ms = 480ms
+        exec("hcitool -i hci0 cmd 0x08 0x0006 00 03 00 03 00 00 00 00 00 00 00 00 00 07 00");
+        exec("hcitool -i hci0 cmd 0x08 0x000a 01"); // leadv
+        exec(advert);                               // override Ubuntu crap set on leadv
+        exec("hciconfig hci0 noscan");              // stop more Ubuntu shite
         logXX(advert);
     }
 
