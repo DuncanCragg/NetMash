f18dc952e5e19f644c98d311358aa7594d418d11
DuncanCragg <cyrus@cilux.org>
2015-01-18 10:59:01 +0000

    improve it

--- src/server/vm2/PiBeaconTemperature.java
+++ src/server/vm2/PiBeaconTemperature.java
@@ -8,13 +8,13 @@ import cyrus.forest.*;
 
 import static cyrus.lib.Utils.*;
 
-/** Class to fetch the temperature from an I2C sensor on a Raspberry Pi and broadcast its URL through a BLE beacon.
+/** Class to fetch the soil moisture level detected by a sensor on a Raspberry Pi and broadcast its URL through a BLE beacon.
   */
 public class PiBeaconTemperature extends CyrusLanguage {
 
     public PiBeaconTemperature(){
-        super("{ is: editable 3d notice temperature sensor\n"+
-              "  title: \"Temperature Sensor\"\n"+
+        super("{ is: editable 3d notice soil moisture sensor\n"+
+              "  title: \"Soil Moisture Sensor\"\n"+
               "  text: waiting..\n"+
               "  rotation: 0 45 0\n"+
               "  scale: 1 1 1\n"+
@@ -24,8 +24,6 @@ public class PiBeaconTemperature extends CyrusLanguage {
     }
 
     private boolean running=false;
-
-    double T=0f;
 
     public void evaluate(){
         if(!running){ running=true;
@@ -40,6 +38,7 @@ public class PiBeaconTemperature extends CyrusLanguage {
     int scaleFactor=28;
     int[] measurements=new int[20];
     int i=0;
+    double avg;
 
     void doit(){
         for(i=0; i< 20; i++) measurements[i]=0;
@@ -52,20 +51,25 @@ public class PiBeaconTemperature extends CyrusLanguage {
 
             long startTime=System.nanoTime();
             int waitCount; for(waitCount=0; PiUtils.getGPIOVal("4")=='0' && waitCount<2000; waitCount++);
-            if(PiUtils.getGPIOVal("4")!='1') continue;
             int measurement=(int)(System.nanoTime()-startTime)/1000000;
+            logXX(waitCount, measurement);
+
+            if(PiUtils.getGPIOVal("4")!='1' ||
+               waitCount==2000              ||
+               measurement<0                ||
+               measurement>300                   ) continue;
 
             measurements[i]=measurement; i++; if(i==20) i=0;
             int average=0; for(int n=0; n<20; n++) average+=measurements[n]; average/=scaleFactor;
             if(average>100) average=100;
 
-            String measurementsAsString=""; for(int n=0; n<20; n++) measurementsAsString+=" "+measurements[n];
-            logXX(waitCount, measurement, measurementsAsString, average);
+            String measurementsAsString="["; for(int n=0; n<20; n++) measurementsAsString+=" "+measurements[n];
+            logXX(measurementsAsString+" ]", average);
 
-            T=average;
+            avg=average;
             new Evaluator(this){ public void evaluate(){
-                contentDouble("soil-moisture", T);
-                content("text", String.format("Soil Moisture: %.0f %%", T));
+                contentDouble("soil-moisture", avg);
+                content("text", String.format("Soil Moisture: %.0f %%", avg));
             }};
         }
     }
